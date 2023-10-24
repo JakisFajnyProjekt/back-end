11a1bb67d86cadbd432b46d84c11409a65c1be5b
FuuKowatty <miedzio.2000@gmail.com>
2023-10-24 13:04:41 +0200

    fix: delete cascade from token
    while token is deleted it should not delete user

--- src/main/java/com/pl/token/Token.java
+++ src/main/java/com/pl/token/Token.java
@@ -15,7 +15,7 @@ import jakarta.persistence.*;
         private String token;
         @Enumerated(EnumType.STRING)
         private TokenType tokenType = TokenType.BEARER;
-        @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+        @ManyToOne(fetch = FetchType.LAZY)
         @JoinColumn(name = "user_id")
         private User user;
         public Token() {
