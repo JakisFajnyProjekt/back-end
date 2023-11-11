1210519b0e1ff3debe51cbc3be87f93c9ecbee64
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2023-11-11 19:33:46 +0100

    feature: restaurant controller test class

--- src/test/java/com/pl/controller/RestaurantControllerTest.java
+++ src/test/java/com/pl/controller/RestaurantControllerTest.java
@@ -12,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -40,7 +42,13 @@ public class RestaurantControllerTest {
         address = new Address("15","street","city","postalCode");
         restaurant = new Restaurant("restaurant",address);
         restaurantDTO = new RestaurantDTO("restaurant",address.getId());
+    }
 
+    @BeforeEach
+    void setMockMvc() {
+        mockMvc = MockMvcBuilders.standaloneSetup(restaurantController)
+                .setMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper))
+                .build();
     }
 
 }
