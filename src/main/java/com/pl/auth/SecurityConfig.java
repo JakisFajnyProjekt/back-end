d7e5afcf2cb6552ef92c8f03880d81c4a524746f
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2023-12-18 22:39:51 +0100

    refactor: security config cors

--- src/main/java/com/pl/auth/SecurityConfig.java
+++ src/main/java/com/pl/auth/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.pl.auth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -10,6 +11,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +24,21 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler LogoutHandler;
+
+    @Value("${cors.allowedOrigin}")
+    private String allowedOrigin;
+
+    @Value("${cors.allowedMethods}")
+    private String allowedMethods;
+
+    @Value("${cors.allowedHeaders}")
+    private String allowedHeaders;
+
+    @Value("${cors.allowedCredentials}")
+    private boolean allowedCredentials;
+
+    @Value("${cors.corsConfiguration}")
+    private String corsConfiguration;
 
     public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationProvider authenticationProvider, LogoutHandler logoutHandler) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
@@ -29,9 +50,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
+                .requestMatchers(corsConfiguration)
+                .permitAll()
+
 //                    .requestMatchers("/api/**")
 //                    .permitAll() <--- for tests
 
@@ -63,6 +89,17 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigin));
+        configuration.setAllowedMethods(Arrays.asList(allowedMethods));
+        configuration.setAllowedHeaders(Arrays.asList(allowedHeaders));
+        configuration.setAllowCredentials(allowedCredentials);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration(corsConfiguration, configuration);
+        return source;
     }
 
 
